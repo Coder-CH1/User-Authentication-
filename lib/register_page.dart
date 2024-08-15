1565d89f692c453a148d5b99895a1fc1ca39b23b
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-08-15 07:25:29 +0100

    added

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -19,6 +19,119 @@ class _RegisterPageState extends State<RegisterPage> {
           color: Colors.white,
         )),
       ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 100),
+        child: Column(
+          children: [
+            Text('Name', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),),
+            SizedBox(
+             height: 10,
+            ),
+            SizedBox(
+              width: 300,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: TextField(
+                  keyboardType: TextInputType.name,
+                  maxLength: 20,
+                  style: const TextStyle(
+                    fontSize: 18,
+                      color: Colors.black,
+                  ),
+                  decoration: const InputDecoration(
+                    hintText: 'Enter name',
+                    hintStyle: TextStyle(
+                      color: Colors.black12,
+                    ),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                  onChanged: (value) {
+                  },
+                  readOnly: true,
+                  onTap: () async {
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text('Email Address', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              width: 300,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  maxLength: 20,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                  decoration: const InputDecoration(
+                    hintText: 'Enter email address',
+                    hintStyle: TextStyle(
+                      color: Colors.black12,
+                    ),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                  onChanged: (value) {
+                  },
+                  readOnly: true,
+                  onTap: () async {
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text('Password', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              width: 300,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: TextField(
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  maxLength: 20,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                  decoration: const InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: TextStyle(
+                      color: Colors.black12,
+                    ),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                  onChanged: (value) {
+                  },
+                  readOnly: true,
+                  onTap: () async {
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
