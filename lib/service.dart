24c53fa59f41c1c7b858fe2b07f415581d2cceb7
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-08-15 06:51:15 +0100

    added

--- lib/service.dart
+++ lib/service.dart
@@ -1,6 +1,34 @@
 
+import 'dart:html';
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 
 class ApiService {
-  final Dio _dio = Dio();
+  final Dio _dio = Dio( BaseOptions(
+    baseUrl: 'http://localhost:3000',
+    headers: {'Content-Type': 'application/json'},
+  )
+  );
+
+  Future<Map<String, dynamic>> register(String name, String email, String password) async {
+    try {
+      final response = await _dio.post(
+          '',
+        data: {
+            'name': name,
+          'email': email,
+          'password': password
+        }
+          );
+      return response.data as Map<String, dynamic>;
+    } catch (e) {
+      throw Exception('');
+    }
+
+  }
+
+  // Future<Map<String, dynamic>> login(String email, String password) async {
+  //   return;
+  // }
 }
