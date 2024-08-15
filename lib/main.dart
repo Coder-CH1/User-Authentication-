6e4f22fe3ea2becf87c4615c9cfa5e6707a39d89
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-08-15 06:11:12 +0100

    added

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:authentication/register_page.dart';
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: UserAuthenticationPage(),
+    );
+  }
+}
+
+class UserAuthenticationPage extends StatelessWidget {
+  const UserAuthenticationPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const RegisterPage(),
+              ),
+              );
+            }, child: const Text('Register', style:
+          TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+            color: Colors.black,
+          ),
+          ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.yellowAccent.shade100,
+              minimumSize: const Size(300, 50),
+            ),
+          ),
+
+          ElevatedButton(
+            onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage(),
+              ),
+              );
+            }, child: const Text('Login', style:
+          TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+            color: Colors.black,
+          ),
+          ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.yellowAccent.shade100,
+              minimumSize: const Size(300, 50),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
